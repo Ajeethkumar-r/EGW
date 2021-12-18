12521d74b200022525f64187b7fa3fb4cc3b80d9
Ajeethkumar-r <rajeethkumarind01.com>
2021-12-18 21:53:29 +0530

    feat:setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-center text-2xl font-medium hover:text-white text-blue-500 bg-green-100 p-1 rounded-md hover:bg-green-400 '>
+      HelloWorld
+    </div>
+  );
+}
+
+export default App;
