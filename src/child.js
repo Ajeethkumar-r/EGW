761b176911395b03b5e269939c79881f495e2978
Ajeethkumar-r <rajeethkumarind01.com>
2021-12-20 08:10:00 +0530

    feat: custom btn props p to c

--- src/child.js
+++ src/child.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-const Child = (props) => {
-  const handleChange = (e) => {
-    props.pv(e.target.value);
-  };
+const Child = ({ onClick, nv, rh }) => {
   return (
-    <>
-      <input
+    <div className='space-y-8 mx-auto'>
+      <button
         className='text-center  max-w-min mx-auto text-2xl font-medium hover:text-white text-blue-400 bg-green-100 p-1 px-2 rounded-md hover:bg-green-400 hover:shadow-lg hover:shadow-green-500 outline-none border-x-green-600 '
-        onChange={handleChange}
-      />
-    </>
+        onClick={onClick}
+      >
+        Button {nv}
+      </button>
+      <div
+        className='text-center  max-w-min mx-auto text-2xl font-medium text-white  bg-red-500 p-1 px-2 rounded-md hover:bg-red-600 hover:shadow-lg hover:shadow-red-500 outline-none border-x-red-600'
+        onClick={rh}
+      >
+        Reset
+      </div>
+    </div>
   );
 };
 
--- src/parent.js
+++ src/parent.js
@@ -1,18 +1,26 @@
+import { click } from '@testing-library/user-event/dist/click';
 import React, { useState } from 'react';
 import Child from './child';
 
 const Parent = (props) => {
-  const [newValue, setNewValue] = useState('');
-  const callabacks = (childData) => {
-    setNewValue(childData);
+  const [newValue, setNewValue] = useState(0);
+  const [reset, setReset] = useState(0);
+  const clickHandler = () => {
+    setNewValue(newValue + 1);
+  };
+  const resetHandler = () => {
+    setReset(0);
+    setNewValue(reset);
   };
   return (
     <div className='flex space-x-5 '>
-      <div>{<Child pv={callabacks} />}</div>
-      <div className='text-2xl p-1 font-medium text-white bg-indigo-800 px-2 rounded-md text-center '>
+      <div>
+        {<Child onClick={clickHandler} rh={resetHandler} nv={newValue} />}
+      </div>
+      {/* <div className='text-2xl p-1 font-medium text-white bg-indigo-800 px-2 rounded-md text-center '>
         {' '}
         {newValue}{' '}
-      </div>
+      </div> */}
     </div>
   );
 };
